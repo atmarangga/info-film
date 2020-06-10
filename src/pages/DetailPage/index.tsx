4c2825de74b63acb9cf2e4240e99ad2427c6e6bd
Atmaji Rangga Adinugroho <atmaji.rangga@gmail.com>
2020-06-11 00:07:20 +0700

    Added index files for each pages

---
+++ src/pages/DetailPage/index.tsx
@@ -0,0 +1,9 @@
+import React, { PureComponent } from "react";
+
+interface Props {}
+
+export default class DetailPage extends PureComponent<Props> {
+  render() {
+    return <>This should be detail pages.</>;
+  }
+}
---
+++ src/pages/HomePage/index.tsx
@@ -0,0 +1,14 @@
+import React, {PureComponent} from 'react';
+
+interface Props {
+    
+}
+export default class HomePage extends PureComponent <Props> {
+    render(){
+        return(
+            <>
+            This should be homepage
+            </>
+        );
+    }
+}
